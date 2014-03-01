f90df33e1a743ff6ab26f9ceaa4d8b3300ad9eeb
Eric Fay <icanhasfay@gmail.com>
2014-03-01 17:08:11 -0500

    Added CLI param functionality.

--- sqrt.py
+++ sqrt.py
@@ -1,12 +1,20 @@
 #Computing Square Roots - Recursive Implementation of the Babylonian method
 #http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+import sys
 listOfNumbers=[]
+
+def usuage():
+	print "Usage:"
+	print "sqrt.py numberToSqrt numberOfSigFigs\n"
+	print "For example:"
+	print "sqrt.py 12345 6\n"
+	sys.exit(1)
 
 def babSqrt(number, sigDig, counter=0):
 	inflatedSigDig = sigDig*100
-	
+
 	half = 0.50
-	
+
 	if counter==0:
 		global firstNumber
 		firstNumber=number
@@ -23,5 +31,8 @@ def babSqrt(number, sigDig, counter=0):
 			#print calculatedNumber
 			return babSqrt(calculatedNumber, sigDig, counter+1)
 
+ 
+if len(sys.argv)!=3:
+	print usuage()
 
-print babSqrt(125348,6)
+print babSqrt(int(sys.argv[1]),int(sys.argv[2]))
